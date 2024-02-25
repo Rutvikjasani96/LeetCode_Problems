785fa983501ee89b43225a6cd7b37ddd4c0973b4
Rutvik_Jasani <Rutvikjasani96@users.noreply.github.com>
2024-02-25 13:40:41 +0530

    Problem 509 Completed.

---
+++ src/Problem_509.java
@@ -0,0 +1,12 @@
+public class Problem_509 {
+//    Fibonacci Number
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
