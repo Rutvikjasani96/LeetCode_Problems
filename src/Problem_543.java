4f79a18897d176f4886e3196577cd7d6c03e15fa
Rutvik_Jasani <Rutvikjasani96@users.noreply.github.com>
2024-03-01 12:15:27 +0530

    Problem 543 Completed.

--- src/Problem_543.java
+++ src/Problem_543.java
@@ -1,17 +1,17 @@
 
 //Definition for a binary tree node.
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+//public class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//    TreeNode() {}
+//    TreeNode(int val) { this.val = val; }
+//    TreeNode(int val, TreeNode left, TreeNode right) {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
 
 public class Problem_543 {
 //    Diameter of Binary Tree
