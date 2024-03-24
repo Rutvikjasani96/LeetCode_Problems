ebca61739c831ee78192a74894ef0179a6837d43
Rutvik_Jasani <Rutvikjasani96@users.noreply.github.com>
2024-03-24 23:52:47 +0530

    Problem 287 Completed.

---
+++ src/Problem_287.java
@@ -0,0 +1,13 @@
+public class Problem_287 {
+//    Find the Duplicate Number
+    public int findDuplicate(int[] nums) {
+        boolean[] contains = new boolean[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(contains[nums[i]-1]){
+                return nums[i];
+            }
+            contains[nums[i]-1]=true;
+        }
+        return 0;
+    }
+}
