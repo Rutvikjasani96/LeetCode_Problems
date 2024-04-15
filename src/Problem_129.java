03a46c2311d5717837254bd5fe8bdf5f9d882049
Rutvik_Jasani <Rutvikjasani96@users.noreply.github.com>
2024-04-15 14:28:21 +0530

    Problem 129 Completed.

---
+++ src/Problem_129.java
@@ -0,0 +1,19 @@
+public class Problem_129 {
+//    Sum Root to Leaf Numbers
+    int ans = 0;
+    public int sumNumbers(TreeNode root) {
+        helper(root,0);
+        return ans;
+    }
+    void helper(TreeNode root,int sum){
+        if(root==null){
+            return;
+        }
+        sum = sum * 10 + root.val;
+        if(root.left==null && root.right==null){
+            ans = ans + sum;
+        }
+        helper(root.left,sum);
+        helper(root.right,sum);
+    }
+}
